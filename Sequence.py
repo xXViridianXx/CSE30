71ebb2a57ddc0b766096bc1489586ee1f300f865
xXViridianXx <43630346+xXViridianXx@users.noreply.github.com>
2021-04-04 11:04:46 -0700

    finished comparison

---
+++ Sequence.py
@@ -0,0 +1,38 @@
+import sys
+
+
+
+def power_remainder(n, b, r):
+    i = 1
+    while True:
+        if i**n % b == r:
+            yield i**n
+        i+=1
+
+def common_terms(g, h):
+
+    x = next(g)
+    y = next(g)
+
+    while True:
+        if y == x:
+            yield y
+            x = next(g)
+        if x < y:
+            x = next(g)
+        if y < x:
+            y = next(h)
+
+if __name__=='__main__':
+
+    A = power_remainder(2, 3, 1)
+    B = power_remainder(3, 5, 4)
+    C = common_terms(power_remainder(2, 3, 1), power_remainder(3, 5, 4))
+
+    print()
+    for i in range(15):
+        s = " {0:<12}{1:<12}{2:<12}".format(next(A), next(B), next(C))
+        print(s)
+        # end
+    print()
+# end
