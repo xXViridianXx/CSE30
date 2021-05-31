e0af3975f6649364f0cb524951c8da508fb07de8
xXViridianXx <43630346+xXViridianXx@users.noreply.github.com>
2021-05-31 11:33:12 -0700

    finished pa6

--- GraphColoring.py
+++ GraphColoring.py
@@ -1,81 +1,56 @@
+#------------------------------------------------------------------------------
+#  Aniket Pratap
+#  1825275
+#  CSE 30-02 Spring 2021
+#  pa6
+#  GraphColoring.py
+#-----------------------------------------------------------------------------
+from os import initgroups
 from graph import *
 import sys
 
 def CheckProperColoring(G):
-    different = 0
     same = 0
     for x, y in G._edges:
-        if G._color[x] != G._color[y]:
-            different += 1
-        if G._color == G._color[y]:
+        if G._color[x] == G._color[y]:
             same += 1
     if same > 0:
         return False
-    if different > 0 and same == 0:
+    if same == 0:
         return True
 
 def getVertices(inputFile):
-    verticeNumber = inputFile[0]
-    inputFile = inputFile[1:]
-    vertices = set()
-    for i in inputFile:
-        vertices.add(i)
+    if inputFile == []:
+        return []
+    else:
+        verticeNumber = inputFile[0]
+        inputFile = inputFile[1:]
+        vertices = set()
+        for i in inputFile:
+            vertices.add(i)
 
-    return vertices
-
+        return vertices
 
 def getEdges(inputFile):
-    verticeNumber = inputFile[0]
-    inputFile = inputFile[1:]
-    edgeList = []
-    while len(inputFile) > 0:
-        edgeList.append((inputFile[0], inputFile[1]))
-        inputFile = inputFile[2:]
-    
-    return edgeList
-
-def printColorsUsed(colorDictionary):
-    maxColoredVert = max(colorDictionary, key=colorDictionary.get)
-    maxColor = colorDictionary[maxColoredVert]
-    colorsUsed = set()
-    for i in range(1,maxColor + 1):
-        colorsUsed.add(i)
-    return f'{maxColor} colors used: {colorsUsed}'
-
-
-
-
-
-
-
-
+    if inputFile == []:
+        return []
+    if len(inputFile) == 1:
+        return None
+    else:
+        verticeNumber = inputFile[0]
+        inputFile = inputFile[1:]
+        edgeList = []
+        while len(inputFile) > 0:
+            edgeList.append((inputFile[0], inputFile[1]))
+            inputFile = inputFile[2:]
+        
+        return edgeList
 
 def usage():
     sys.stderr.write('Usage: $ python3 GraphColoring.py <input file> <output file>')
     
-
-
 # end
 def main():
- # check command line arguments and open files
-
- # read each line of input file
-
- # get number of vertices on first line, create vertex list
-
- # create edge list from remaining lines
-
- # create graph G
-
- # Determine a proper coloring of G and print it to the output file
-
-    """
-    # Check that the coloring is correct
-    print(file=outfile)
-    msg = 'coloring is proper: {}'.format(CheckProperColoring(G))
-    print(msg, file=outfile )
-
-    """
     if len(sys.argv) != 3:
         usage()
     else:
@@ -92,68 +67,27 @@ def main():
                 # print(A, file=outfile)
                 # print(a, file=outfile)
                 # print('', file=outfile)
-            
             G = Graph(getVertices(R), getEdges(R))
-            G.find_best(getVertices(R))
-            colored = G.Color()
-            A = printColorsUsed(colored)
-            print('', file=outfile)
-            print(A, file=outfile)
-            print('', file=outfile)
-            print('vertex    color', file=outfile)
-            print('----------------', file=outfile)
-            for vertice, color in colored.items():
-                if 0 < vertice < 10:
-                    print(f' {vertice}         {color}         ', file=outfile)
-                elif 10 <= vertice < 100:
-                    print(f' {vertice}        {color}         ', file=outfile)
-                elif 100 <= vertice < 1000:
-                    print(f' {vertice}       {color}         ', file=outfile)
-            # print('')
-            # print(A)
-            # print('')
-            # print('vertex    color')
-            # print('----------------')
-            # for vertice, color in colored.items():
-            #     if 0 < vertice < 10:
-            #         print(f' {vertice}         {color}         ')
-            #     elif 10 <= vertice < 100:
-            #         print(f' {vertice}        {color}         ')
-            #     elif 100 <= vertice < 1000:
-            #         print(f' {vertice}       {color}         ')
-            #print(CheckProperColoring(G))
-
-            
-
-
+            if R == []:
+                print(G._color, file=outfile)
+            else:
+                G = Graph(getVertices(R), getEdges(R))
+                G.find_best(getVertices(R))
+                colored = G.Color()
+                print(f'{len(colored)} colors used: {colored}', file=outfile)
+                print('', file=outfile)
+                print('vertex    color', file=outfile)
+                print('----------------', file=outfile)
+                for vertice, color in G._color.items():
+                    if 0 < vertice < 10:
+                        print(f' {vertice}         {color}         ', file=outfile)
+                    elif 10 <= vertice < 100:
+                        print(f' {vertice}        {color}         ', file=outfile)
+                    elif 100 <= vertice < 1000:
+                        print(f' {vertice}       {color}         ', file=outfile)
         except FileNotFoundError as e:
             print(e, file=sys.stderr)
             usage()
 
 if __name__ == '__main__':
     main()
-
-# V = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
-# E = [(1,7),(7,12),(7,5),(7,3),(3,2),(5,2),(5,11),(6,9),(6,10),(10,4),(10,15),(15,14),(14,13),(9,13),(13,8)]
-# G = Graph(V,E)
-
-# print()
-# print(G.vertices)
-# print(G.edges)
-# print(G.find_best(V))
-# print(G.Color())
-# print(G.get_color(5))
-# print(CheckProperColoring(G))
-
-# V = [1,2,3,4,5,6,7]
-# E = [(1,2),(2,5),(2,3),(2,6),(2,4),(5,4),(5,3),(3,6),(3,7),(7,4),(4,3)]
-# G = Graph(V,E)
-
-# print()
-# print(G.vertices)
-# print(G.edges)
-# print(G.find_best(V))
-# print(G.Color())
-# #print(G.get_color(5))
-# print(CheckProperColoring(G))
-# # end
--- graph.py
+++ graph.py
@@ -1,4 +1,11 @@
-# # from queue import *
+#------------------------------------------------------------------------------
+#  Aniket Pratap
+#  1825275
+#  CSE 30-02 Spring 2021
+#  pa6
+#  graph.py
+#-----------------------------------------------------------------------------
+from queue import *
 
 class Graph:
     def __init__(self, V, E):
@@ -7,9 +14,10 @@ class Graph:
         # basic attributes
         self._vertices = list(V)
         self._vertices.sort()
+        self._pallete = [c for c in range(1, len(self._vertices) +1)]
         self._edges = list(E)
         self._adj = {x: list() for x in V}
-        self._color = {c: 0 for c in V}
+        self._color = {c: None for c in V}
         self._ecs = {e: [] for e in V}
         for e in E:
             x, y = tuple(e)
@@ -18,11 +26,6 @@ class Graph:
             self._adj[x].sort()
             self._adj[y].sort()
         # end
-
-        # additional attributes
-        self._component = {x: None for x in V}  # for findComponents()
-        self._distance = {x: None for x in V}  # for BFS()
-        self._predecessor = {x: None for x in V}  # for BFS()
 
     @property
     def vertices(self):
@@ -36,104 +39,56 @@ class Graph:
         """Return the list of edges of self."""
         return self._edges
 
-    # end
-
-    def __str__(self):
-        """Return a string representation of self."""
-        s = ''
-        for x in self.vertices:
-            a = str(self._adj[x])
-            s += '{}: {}\n'.format(x, a[1:-1])
-        # end
-        return s
-
-    # end
 
     def find_best(self, L):
-        vertices = L
-        highest_degree = 0
+        L = list(L)
+        vertices = L # [1:]
         sorted_vertices = []
-        while len(vertices) != 0:
-            max = 0
-            for i in vertices:
-                if self._color[i] == 0 and len(self._adj[i]) > max:
-                    max = len(self._adj[i])
-                    highest_degree = i
-                if len(self._adj[i]) == max:
-                    highest_degree == min(highest_degree, i)
+        adjacent = {x: len(self._adj[x]) for x in vertices}
+        if len(L) > 0:
+            while len(adjacent) > 0:
+                mostAdj = max(adjacent, key=adjacent.get)
+                sorted_vertices.append(mostAdj)
+                adjacent.pop(mostAdj)
+        else:
+            sorted_vertices = []
 
-            sorted_vertices.append(highest_degree)
-            vertices.remove(highest_degree)
         return sorted_vertices
 
     def Color(self):
-        colors = []
-        #self._color = self._color
-        vertices = self._vertices
-        finished_vertices = []
-
-        for c in range(1, len(vertices) + 1):
-            colors.append(c)
-        sorted_degree = self.find_best(vertices)
-        no_constraints = sorted_degree[0]
-
-        while len(sorted_degree) > 0:
-            available = []
-            if sorted_degree[0] == no_constraints:
-                self._color[sorted_degree[0]] = 1
-                for i in self._adj[sorted_degree[0]]:
-                    self._ecs[i].append(1)
-                finished_vertices.append(sorted_degree[0])
+        sorted_degree = self.find_best(self._vertices)
+        if sorted_degree == []:
+            return {}
+        if len(sorted_degree) == 1:
+            return {1}
+        else:
+            finishedVertices = []
+            colors = self._pallete
+            while len(sorted_degree) > 0:
+                available = []
+                if self._ecs[sorted_degree[0]] == [] and self._color[sorted_degree[0]] == None:
+                    self._color[sorted_degree[0]] = 1
+                    for i in self._adj[sorted_degree[0]]:
+                        self._ecs[i].append(1)
+                if (self._color[sorted_degree[0]] == None) and sorted_degree[0] not in finishedVertices:
+                    for i in colors:
+                        if i not in self._ecs[sorted_degree[0]]:
+                            available.append(i)
+                    self._color[sorted_degree[0]] = available[0]
+                    for c in self._adj[sorted_degree[0]]:
+                        if available[0] not in self._ecs[c]:
+                            self._ecs[c].append(available[0])
+                            self._ecs[c].sort()
+                finishedVertices.append(sorted_degree[0])
                 sorted_degree = sorted_degree[1:]
-            if sorted_degree[0] != no_constraints:
-                for i in colors:
-                    if i not in self._ecs[sorted_degree[0]]:
-                        available.append(i)
-                self._color[sorted_degree[0]] = available[0]
-                for i in self._adj[sorted_degree[0]]:
-                    if available[0] not in self._ecs[i] and self._color[i] != available[0]:
-                        self._ecs[i].append(available[0])
-                        self._ecs[i].sort()
-                finished_vertices.append(sorted_degree[0])
-                sorted_degree = sorted_degree[1:]
-
-        return self._color
-
-    def get_color(self, x):
+            colorDictionary = self._color
+            maxColoredVert = max(colorDictionary, key=colorDictionary.get)
+            maxColor = colorDictionary[maxColoredVert]
+            colorsUsed = set()
+            for i in range(1,maxColor + 1):
+                colorsUsed.add(int(i))
+            return colorsUsed
+                
+    def getColor(self, x):
         return self._color[x]
-
-
-# V = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-# E = [(1, 2), (1, 6), (6, 2), (2, 7), (6, 7), (2, 3), (2, 5), (7, 5), (7, 8), (3, 4), (4, 5), (4, 9), (9, 5), (9, 8)]
-# G = Graph(V, E)
-#
-# print(G.vertices)
-# print(G.edges)
-# print(G.find_best(V))
-# print(G.Color())
-
-
-# V = [1,2,3,4,5,6]
-# E = [(1,2),(1,4),(2,3),(2,4),(2,5),(3,5),(3,6),(4,5),(5,6)]
-# G = Graph(V,E)
-#
-# print()
-# print(G.vertices)
-# print(G.edges)
-# print(G.find_best(V))
-# print(G.Color())
-# print(G.get_color(5))
-
-# V = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
-# E = [(1,7),(7,12),(7,5),(7,3),(3,2),(5,2),(5,11),(6,9),(6,10),(10,4),(10,15),(15,14),(14,13),(9,13),(13,8)]
-# G = Graph(V,E)
-
-# print()
-# print(G.vertices)
-# print(G.edges)
-# print(G.find_best(V))
-# print(G.Color())
-# print(G.get_color(5))
-
-
 
