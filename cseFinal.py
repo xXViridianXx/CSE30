583142a7cebd2c0850c722b7161df9ab90a8f564
xXViridianXx <43630346+xXViridianXx@users.noreply.github.com>
2021-06-10 11:50:56 -0700

    bruh

--- cseFinal.py
+++ cseFinal.py
@@ -1,1 +1,23 @@
-print('hello')
+# D = {1: [2, 3], 2: [3], 3: [4], 4: [2]}
+
+
+# # def transpose(D):
+# #     trans = {}
+# #     for key, value in D.items():
+# #         for i in value:
+# #             trans[value] = key
+
+# def transpose(D):
+#     trans = {}
+#     for key, value in D.items():
+#         for i in value:
+#             newList = []
+#             trans[i] = newList
+#             trans[i].append(key)
+#     return trans
+
+
+# print(transpose(D))
+
+a = [1, 2, 3]
+print(max(a))
